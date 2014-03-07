62ef7fce328fd3e508552f5fb501c9b1cd7bebaf
Marc G <marc@marcg.ch>
2014-03-07 11:51:23 +0100

    Check size and block size on creation of a new VDisk

--- VFS/src/ethz/jcd/main/VUtil.java
+++ VFS/src/ethz/jcd/main/VUtil.java
@@ -3,10 +3,14 @@ package ethz.jcd.main;
 import ethz.jcd.main.allocator.Allocator;
 import ethz.jcd.main.blocks.Block;
 import ethz.jcd.main.blocks.Inode;
+import ethz.jcd.main.exceptions.InvalidBlockSize;
+import ethz.jcd.main.exceptions.InvalidSize;
+import ethz.jcd.main.exceptions.VDiskCreationException;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.List;
 
@@ -17,17 +21,33 @@ public class VUtil
 
     public VUtil( String vDiskFile ) throws FileNotFoundException {
         this.vDiskFile = vDiskFile;
-        File fp = new File(this.vDiskFile);
-
-        raf = new RandomAccessFile(fp, "rwd");
+        raf = new RandomAccessFile(vDiskFile, "rwd");
 
         throw new NotImplementedException();
     }
 
-    public VUtil( String vDiskFile, long size, long blockSize ) throws FileNotFoundException {
-        // TODO Create VDisk file
+    public VUtil( String vDiskFile, long size, int blockSize ) throws InvalidSize, InvalidBlockSize, VDiskCreationException {
+        this.vDiskFile = vDiskFile;
 
-        this(vDiskFile);
+        // Check size and blockSize for validity
+        if (size <= 0 || size % blockSize != 0) {
+            throw new InvalidSize();
+        }
+        // TODO: check for minimum block size (to fit at least the superblock)
+        if (blockSize <= 0) {
+            throw new InvalidBlockSize();
+        }
+
+        File fp = new File(this.vDiskFile);
+        try {
+            if (!fp.createNewFile()) {
+                throw new VDiskCreationException();
+            }
+        } catch (IOException e) {
+            throw new VDiskCreationException();
+        }
+
+        throw new NotImplementedException();
     }
 
     /**
