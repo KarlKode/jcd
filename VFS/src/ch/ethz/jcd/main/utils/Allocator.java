c6e2846f1b7bde2433f873bab12fec6c2663c790
Marc G <marc@marcg.ch>
2014-03-13 09:19:09 +0100

    Javadocs, code formatting and removed an unused method

--- VFS/src/ch/ethz/jcd/main/utils/Allocator.java
+++ VFS/src/ch/ethz/jcd/main/utils/Allocator.java
@@ -1,6 +1,5 @@
 package ch.ethz.jcd.main.utils;
 
-import ch.ethz.jcd.main.blocks.BitMapBlock;
 import ch.ethz.jcd.main.blocks.Block;
 import ch.ethz.jcd.main.blocks.SuperBlock;
 import ch.ethz.jcd.main.exceptions.BlockAddressOutOfBoundException;
@@ -10,97 +9,78 @@ import ch.ethz.jcd.main.exceptions.DiskFullException;
  * The Allocator provides an interface to allocate and free Blocks to
  * make sure that no where else BitMapBlock changes are needed to perform.
  * It keeps also the BitMapBlock in sync.
- * <p/>
- * Created by phgamper on 3/12/14.
  */
 public class Allocator
 {
-    private BitMapBlock bitMapBlock;
     private VUtil vUtil;
 
     /**
-     * This constructor creates a new Allocator.
+     * Instantiate a new Allocator
      *
-     * @param vUtil - VUtil used to keep BitMapBlock in sync with the VDisk
-     *              must be initialized correctly
+     * @param vUtil - VUtil of the VDisk the Allocator belongs to
      */
     public Allocator(VUtil vUtil)
     {
         this.vUtil = vUtil;
-        this.bitMapBlock = vUtil.getBitMapBlock();
     }
 
     /**
-     * This method allocates a Block
+     * Allocate a new Block
      *
      * @return the allocated Block
-     * @throws DiskFullException if no unused Block is available
+     * @throws DiskFullException if no more unused Blocks are available
      */
     public Block allocate() throws DiskFullException
     {
         // Get the next free block and set it to used
-        int freeBlockAddress = 0;
+        int freeBlockAddress;
         try
         {
-            freeBlockAddress = bitMapBlock.allocateBlock();
+            freeBlockAddress = vUtil.getBitMapBlock().allocateBlock();
         } catch (BlockAddressOutOfBoundException e)
         {
             throw new DiskFullException();
         }
 
+        Block newBlock = new Block(freeBlockAddress, new byte[vUtil.getSuperBlock().getBlockSize()]);
+
         // Sync
-        vUtil.write(bitMapBlock);
-        return new Block(freeBlockAddress);
+        vUtil.write(vUtil.getBitMapBlock());
+        vUtil.write(newBlock);
+
+        return newBlock;
     }
 
     /**
-     * This method frees a Block
+     * Free a Block
      *
-     * @param block
+     * @param block Block to be freed
      */
     public void free(Block block)
     {
         try
         {
-            bitMapBlock.setUnused(block.getAddress());
+            vUtil.getBitMapBlock().setUnused(block.getAddress());
         } catch (BlockAddressOutOfBoundException e)
         {
             // TODO This should never happen!
             e.printStackTrace();
         }
+
         // Sync
-        vUtil.write(bitMapBlock);
+        vUtil.write(vUtil.getBitMapBlock());
     }
 
     /**
-     * This method checks whether the block address of given Block is used or not
-     *
-     * @param block - Block containing at least the block address
-     * @return True if given Block is free
-     */
-    public boolean isFree(Block block)
-    {
-        try
-        {
-            return bitMapBlock.isUnused(block.getAddress());
-        } catch (BlockAddressOutOfBoundException e)
-        {
-            // This should never happen
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    /**
-     * This method clears and initialize the BitMapBlock.
+     * Completely clear a VDisk and initialize it as empty
      */
     public void format()
     {
-        bitMapBlock.clear();
+        vUtil.getBitMapBlock().clear();
         try
         {
-            bitMapBlock.setUsed(SuperBlock.SUPER_BLOCK_ADDRESS);
-            bitMapBlock.setUsed(SuperBlock.BIT_MAP_BLOCK_ADDRESS);
+            vUtil.getBitMapBlock().setUsed(SuperBlock.SUPER_BLOCK_ADDRESS);
+            vUtil.getBitMapBlock().setUsed(SuperBlock.BIT_MAP_BLOCK_ADDRESS);
         } catch (BlockAddressOutOfBoundException e)
         {
             // This should never happen
@@ -108,6 +88,7 @@ public class Allocator
         }
 
         //TODO Root Directory Block Address
-        vUtil.write(bitMapBlock);
+        vUtil.write(vUtil.getSuperBlock());
+        vUtil.write(vUtil.getBitMapBlock());
     }
 }
