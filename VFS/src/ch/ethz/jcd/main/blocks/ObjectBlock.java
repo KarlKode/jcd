adbd4c5dcdf49dc23099bc7c67fdaa34d9f2c8b2
Marc G <marc@marcg.ch>
2014-03-26 21:24:05 +0100

    Completed a first version of the DirectoryBlock

--- VFS/src/ch/ethz/jcd/main/blocks/ObjectBlock.java
+++ VFS/src/ch/ethz/jcd/main/blocks/ObjectBlock.java
@@ -4,23 +4,18 @@ import ch.ethz.jcd.main.exceptions.InvalidBlockAddressException;
 import ch.ethz.jcd.main.exceptions.InvalidNameException;
 import ch.ethz.jcd.main.exceptions.InvalidTypeException;
 import ch.ethz.jcd.main.utils.FileManager;
-import ch.ethz.jcd.main.utils.VUtil;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
-import java.util.List;
 
-public abstract class ObjectBlock extends Block
+public class ObjectBlock extends Block
 {
     public static final int LENGTH_TYPE = 1;
     public static final int LENGTH_NAME = 63;
     public static final int LENGTH_PARENT_BLOCK_ADDRESS = 4;
-    public static final int LENGTH_CONTENT_SIZE = 8;
     public static final int OFFSET_TYPE = 0;
     public static final int OFFSET_NAME = OFFSET_TYPE + LENGTH_TYPE;
     public static final int OFFSET_PARENT_BLOCK_ADDRESS = OFFSET_NAME + LENGTH_NAME;
-    public static final int OFFSET_CONTENT_SIZE = OFFSET_PARENT_BLOCK_ADDRESS + LENGTH_CONTENT_SIZE;
-    public static final int OFFSET_CHILDREN = OFFSET_CONTENT_SIZE + LENGTH_PARENT_BLOCK_ADDRESS;
+    public static final int OFFSET_CONTENT = OFFSET_PARENT_BLOCK_ADDRESS + LENGTH_PARENT_BLOCK_ADDRESS;
     public static final byte TYPE_DIRECTORY = 0x00;
     public static final byte TYPE_FILE = 0x01;
 
@@ -64,12 +59,4 @@ public abstract class ObjectBlock extends Block
 
         fileManager.writeInt(getBlockOffset(), OFFSET_PARENT_BLOCK_ADDRESS, parent.getBlockAddress());
     }
-
-    public abstract long getSize();
-
-    public abstract List<ObjectBlock> getChildren();
-
-    public abstract void addChild(Block block);
-
-    public abstract void removeChild(Block block);
 }
