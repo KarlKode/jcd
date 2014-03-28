24e6f986cc166d84df3493972f9bd6d214f05d69
Marc G <marc@marcg.ch>
2014-03-28 23:32:24 +0100

    Tests and java docs

--- VFS/src/ch/ethz/jcd/main/blocks/SuperBlock.java
+++ VFS/src/ch/ethz/jcd/main/blocks/SuperBlock.java
@@ -1,11 +1,12 @@
 package ch.ethz.jcd.main.blocks;
 
-import ch.ethz.jcd.main.exceptions.InvalidBlockAddressException;
-import ch.ethz.jcd.main.exceptions.InvalidBlockCountException;
 import ch.ethz.jcd.main.utils.FileManager;
 
 import java.io.IOException;
 
+/**
+ * Super block that stores all metadata of the VFS
+ */
 public class SuperBlock extends Block
 {
     public static final int OFFSET_BLOCK_COUNT = 0;
@@ -14,58 +15,74 @@ public class SuperBlock extends Block
     public static final int BIT_MAP_BLOCK_ADDRESS = 1;
     public static final int DATA_BLOCK_BEGIN_ADDRESS = 2;
 
-    public SuperBlock(FileManager fileManager, int blockAddress) throws InvalidBlockAddressException
+    public SuperBlock(FileManager fileManager, int blockAddress) throws IllegalArgumentException
     {
         super(fileManager, blockAddress);
 
         if (blockAddress != SUPER_BLOCK_ADDRESS)
         {
-            throw new InvalidBlockAddressException();
+            throw new IllegalArgumentException();
         }
     }
 
+    /**
+     * @return block count
+     * @throws IOException
+     */
     public int getBlockCount() throws IOException
     {
         return fileManager.readInt(getBlockOffset(), OFFSET_BLOCK_COUNT);
     }
 
-    public void setBlockCount(int blockCount) throws InvalidBlockCountException, IOException
+    /**
+     * @param blockCount new block count
+     * @throws IOException
+     * @throws IllegalArgumentException if new block count is invalid
+     */
+    public void setBlockCount(int blockCount) throws IOException, IllegalArgumentException
     {
         // Check validity of block count
         // TODO: Block count > number of supported blocks
         if (blockCount < 0)
         {
-            throw new InvalidBlockCountException();
+            throw new IllegalArgumentException();
         }
 
         fileManager.writeInt(getBlockOffset(), OFFSET_BLOCK_COUNT, blockCount);
     }
 
+    /**
+     *
+     * @return block address of root directory block
+     * @throws IOException
+     */
     public int getRootDirectoryBlock() throws IOException
     {
         return fileManager.readInt(getBlockOffset(), OFFSET_ROOT_DIRECTORY_BLOCK);
     }
 
-    public void setRootDirectoryBlock(int rootDirectoryBlockAddress) throws InvalidBlockAddressException, IOException
+    /**
+     *
+     * @param rootDirectoryBlockAddress new root directory block
+     * @throws IOException
+     */
+    public void setRootDirectoryBlock(int rootDirectoryBlockAddress) throws IOException
     {
-        // Check validity of root directory block address
-        if (!isValidBlockAddress(rootDirectoryBlockAddress))
-        {
-            throw new InvalidBlockAddressException();
-        }
-
         fileManager.writeInt(getBlockOffset(), OFFSET_ROOT_DIRECTORY_BLOCK, rootDirectoryBlockAddress);
     }
 
+    /**
+     *
+     * @return block address of first bit map block
+     */
     public int getFirstBitMapBlock()
     {
         return BIT_MAP_BLOCK_ADDRESS;
     }
 
     /**
-     * Get the block blockAddress of the last BitMapBlock of the FS the SuperBlock belongs to
      *
-     * @return block blockAddress of the last BitMapBlock of the FS the SuperBlock belongs to
+     * @return block address of last bit map block
      */
     public int getLastBitMapBlock()
     {
@@ -74,9 +91,7 @@ public class SuperBlock extends Block
     }
 
     /**
-     * Get the block blockAddress of the first Block with real data of the FS the SuperBlock belongs to
-     *
-     * @return block blockAddress of the first Block with real data of the FS the SuperBlock belongs to
+     * @return block address of first block that does not store any filesystem metadata and stores actual content
      */
     public int getFirstDataBlock()
     {
--- VFS/test/ch/ethz/jcd/main/blocks/SuperBlockTest.java
+++ VFS/test/ch/ethz/jcd/main/blocks/SuperBlockTest.java
@@ -1,161 +1,109 @@
 package ch.ethz.jcd.main.blocks;
 
-import ch.ethz.jcd.main.exceptions.InvalidBlockAddressException;
-import ch.ethz.jcd.main.exceptions.InvalidBlockCountException;
-import ch.ethz.jcd.main.exceptions.InvalidBlockSizeException;
+import ch.ethz.jcd.main.utils.FileManager;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.nio.ByteBuffer;
+import java.io.File;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class SuperBlockTest
 {
-    private static final int VDISK_SIZE = SuperBlock.MIN_SUPER_BLOCK_SIZE * 8;
-    private static final int VDISK_BLOCK_SIZE = SuperBlock.MIN_SUPER_BLOCK_SIZE;
-    private static final int VDISK_BLOCK_COUNT = VDISK_SIZE / VDISK_BLOCK_SIZE;
-    private static final int VDISK_ROOT_DIRECTORY_BLOCK = 2;
-    public static final int VDISK_FIRST_BIT_MAP_BLOCK_ADDRESS = SuperBlock.BIT_MAP_BLOCK_ADDRESS;
-    public static final int VDISK_LAST_BIT_MAP_BLOCK_ADDRESS = SuperBlock.BIT_MAP_BLOCK_ADDRESS;
-    public static final int VDISK_FIRST_DATA_BLOCK_ADDRESS = SuperBlock.DATA_BLOCK_BEGIN_ADDRESS;
-    private ByteBuffer buffer;
+    private static final int BLOCK_ADDRESS = 0;
+    private static final int BLOCK_COUNT = 2;
+    private static final int ROOT_DIRECTORY = 1234;
+    private FileManager fileManager;
     private SuperBlock block;
-
-    /**
-     * constants to test whether the constructor sets blockSize, blockCount,
-     * rootDirectoryBlock correctly
-     */
-    private static final byte[] TEST_CONSTRUCTOR = new byte[]{0, 0, 0, 64, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0};
-    private static final int TEST_CONSTRUCTOR_BLOCK_SIZE = 64;
-    private static final int TEST_CONSTRUCTOR_BLOCK_COUNT = 256;
-    private static final int TEST_CONSTRUCTOR_ROOT_DIRECTORY_BLOCK_ADDRESS = 2;
-
 
     @Before
     public void setUp() throws Exception
     {
-        byte[] bytes = new byte[VDISK_BLOCK_SIZE];
-        buffer = ByteBuffer.wrap(bytes);
-        buffer.putInt(SuperBlock.OFFSET_BLOCK_SIZE, VDISK_BLOCK_SIZE);
-        buffer.putInt(SuperBlock.OFFSET_BLOCK_COUNT, VDISK_BLOCK_COUNT);
-        buffer.putInt(SuperBlock.OFFSET_ROOT_DIRECTORY_BLOCK, VDISK_ROOT_DIRECTORY_BLOCK);
-        block = new SuperBlock(bytes);
-    }
-
-    @Test(expected = InvalidBlockSizeException.class)
-    public void testConstructorInvalid0() throws Exception
-    {
-        block = new SuperBlock(new byte[0]);
-    }
-
-    @Test(expected = InvalidBlockSizeException.class)
-    public void testConstructorInvalid1() throws Exception
-    {
-        block = new SuperBlock(null);
+        File tmpFile = File.createTempFile("test", "vfs");
+        tmpFile.deleteOnExit();
+        fileManager = new FileManager(tmpFile);
+        // Block count
+        fileManager.writeInt(0, SuperBlock.OFFSET_BLOCK_COUNT, BLOCK_COUNT);
+        // Root directory
+        fileManager.writeInt(0, SuperBlock.OFFSET_ROOT_DIRECTORY_BLOCK, ROOT_DIRECTORY);
+        block = new SuperBlock(fileManager, BLOCK_ADDRESS);
     }
 
     @Test
-    public void testConstructor() throws InvalidBlockSizeException
+    public void testConstructor() throws Exception
     {
-        SuperBlock superBlock = new SuperBlock(TEST_CONSTRUCTOR);
-        assertEquals(TEST_CONSTRUCTOR_BLOCK_SIZE, superBlock.getBlockSize());
-        assertEquals(TEST_CONSTRUCTOR_BLOCK_COUNT, superBlock.getBlockCount());
-        assertEquals(TEST_CONSTRUCTOR_ROOT_DIRECTORY_BLOCK_ADDRESS, superBlock.getRootDirectoryBlock());
+        new SuperBlock(fileManager, 0);
+        try
+        {
+            new SuperBlock(null, 0);
+            fail("Exception was expected for invalid file manager");
+        } catch (IllegalArgumentException e)
+        {
+        }
+        try
+        {
+            new SuperBlock(fileManager, -1);
+            fail("Exception was expected for invalid block address");
+        } catch (IllegalArgumentException e)
+        {
+        }
+        try
+        {
+            new SuperBlock(fileManager, 1);
+            fail("Exception was expected for invalid super block address");
+        } catch (IllegalArgumentException e)
+        {
+        }
     }
-
-    @Test(expected = InvalidBlockSizeException.class)
-    public void testSetBlockSize() throws Exception
-    {
-        int blockSize;
-
-        blockSize = VDISK_BLOCK_SIZE;
-        block.setBlockSize(blockSize);
-        assertEquals(blockSize, block.getBlockSize());
-        assertEquals(blockSize, buffer.getInt(SuperBlock.OFFSET_BLOCK_SIZE));
-
-        blockSize = VDISK_BLOCK_SIZE * 2;
-        block.setBlockSize(blockSize);
-        assertEquals(blockSize, block.getBlockSize());
-        assertEquals(blockSize, buffer.getInt(SuperBlock.OFFSET_BLOCK_SIZE));
-
-        blockSize = VDISK_SIZE;
-        block.setBlockSize(blockSize);
-        assertEquals(blockSize, block.getBlockSize());
-        assertEquals(blockSize, buffer.getInt(SuperBlock.OFFSET_BLOCK_SIZE));
-
-        blockSize = 0;
-        block.setBlockSize(blockSize);
-        assertEquals(blockSize, block.getBlockSize());
-        assertEquals(blockSize, buffer.getInt(SuperBlock.OFFSET_BLOCK_SIZE));
-    }
-
-    @Test
-    public void testGetBlockSize() throws Exception
-    {
-        assertEquals(VDISK_BLOCK_SIZE, block.getBlockSize());
-    }
-
-    @Test(expected = InvalidBlockCountException.class)
-    public void testSetBlockCount() throws Exception
-    {
-        int blockCount;
-
-        blockCount = VDISK_BLOCK_COUNT;
-        block.setBlockCount(blockCount);
-        assertEquals(blockCount, block.getBlockCount());
-        assertEquals(blockCount, buffer.getInt(SuperBlock.OFFSET_BLOCK_COUNT));
-
-        blockCount = VDISK_BLOCK_SIZE / VDISK_BLOCK_SIZE;
-        block.setBlockCount(blockCount);
-        assertEquals(blockCount, block.getBlockCount());
-        assertEquals(blockCount, buffer.getInt(SuperBlock.OFFSET_BLOCK_COUNT));
-
-        blockCount = 0;
-        block.setBlockCount(blockCount);
-    }
-
     @Test
     public void testGetBlockCount() throws Exception
     {
-        assertEquals(VDISK_BLOCK_COUNT, block.getBlockCount());
+        assertEquals(BLOCK_COUNT, block.getBlockCount());
     }
 
-    @Test(expected = InvalidBlockAddressException.class)
-    public void testSetRootDirectoryBlock() throws Exception
+    @Test
+    public void testSetBlockCount() throws Exception
     {
-        int rootDirectoryBlockAddress;
-
-        rootDirectoryBlockAddress = VDISK_ROOT_DIRECTORY_BLOCK;
-        block.setRootDirectoryBlock(rootDirectoryBlockAddress);
-        assertEquals(rootDirectoryBlockAddress, block.getRootDirectoryBlock());
-        assertEquals(rootDirectoryBlockAddress, buffer.getInt(SuperBlock.OFFSET_ROOT_DIRECTORY_BLOCK));
-
-        rootDirectoryBlockAddress = -1;
-        block.setRootDirectoryBlock(rootDirectoryBlockAddress);
+        int newBlockCount = BLOCK_COUNT + 1;
+        block.setBlockCount(newBlockCount);
+        assertEquals(newBlockCount, block.getBlockCount());
+        try {
+            block.setBlockCount(-1);
+            fail("Exception was expected for invalid block count");
+        } catch (IllegalArgumentException e) {
+        }
     }
 
     @Test
     public void testGetRootDirectoryBlock() throws Exception
     {
-        assertEquals(VDISK_ROOT_DIRECTORY_BLOCK, block.getRootDirectoryBlock());
+        assertEquals(ROOT_DIRECTORY, block.getRootDirectoryBlock());
+    }
+
+    @Test
+    public void testSetRootDirectoryBlock() throws Exception
+    {
+        int newRootDirectory = ROOT_DIRECTORY + 1;
+        block.setRootDirectoryBlock(newRootDirectory);
+        assertEquals(newRootDirectory, block.getRootDirectoryBlock());
     }
 
     @Test
     public void testGetFirstBitMapBlock() throws Exception
     {
-        assertEquals(VDISK_FIRST_BIT_MAP_BLOCK_ADDRESS, block.getFirstBitMapBlock());
+        assertEquals(SuperBlock.BIT_MAP_BLOCK_ADDRESS, block.getFirstBitMapBlock());
     }
 
     @Test
     public void testGetLastBitMapBlock() throws Exception
     {
-        assertEquals(VDISK_LAST_BIT_MAP_BLOCK_ADDRESS, block.getLastBitMapBlock());
+        assertEquals(SuperBlock.BIT_MAP_BLOCK_ADDRESS, block.getLastBitMapBlock());
     }
 
     @Test
     public void testGetFirstDataBlock() throws Exception
     {
-        assertEquals(VDISK_FIRST_DATA_BLOCK_ADDRESS, block.getFirstDataBlock());
+        assertEquals(SuperBlock.DATA_BLOCK_BEGIN_ADDRESS, block.getFirstDataBlock());
     }
 }
