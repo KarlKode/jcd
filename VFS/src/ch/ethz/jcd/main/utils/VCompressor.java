261e996ea814415ad1a84206521ec96fcf8176ab
Philipp Gamper <phgamper@student.ethz.ch>
2014-04-29 02:40:29 +0200

    Compress0r done

--- VFS/src/ch/ethz/jcd/main/utils/VCompressor.java
+++ VFS/src/ch/ethz/jcd/main/utils/VCompressor.java
@@ -2,114 +2,326 @@ package ch.ethz.jcd.main.utils;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.BitSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 
+/**
+ * This class implements entropy compression using the Huffan coding algorithm
+ * with a block size of two bits.
+ */
 public class VCompressor
 {
-    public static final int DECOMPRESSED_WORD_SIZE = 16; // in bytes
-    public static final int COMPRESSED_WORD_SIZE = (Double.SIZE / 8) + 1;
+    /**
+     * Code values used according to the occurrence probability
+     * e.g.
+     * 0
+     * 00 - 0.4 ------------------------+------+
+     * |
+     * 10      1   |
+     * 11 - 0.3 ----------------+-------+
+     * |
+     * 110     11  |
+     * 01 - 0.2 --------+-------+
+     * |
+     * 111 |
+     * 10 - 0.1 --------+
+     */
+    public static final int[] CODE_BOOK = {0, 2, 6, 7};
 
+    /**
+     * Compresses the given byte array
+     *
+     * @param bytes to compress
+     *
+     * @return compressed bytes
+     */
     public byte[] compress(byte[] bytes)
     {
-        int len = (int) Math.ceil(bytes.length / DECOMPRESSED_WORD_SIZE) * COMPRESSED_WORD_SIZE;
-        byte[] buf = new byte[len];
-        ByteBuffer buffer = ByteBuffer.wrap(buf);
-        int k = 0;
+        Dictionary dict = new Dictionary(bytes);
+        BitCompressStream bits = new BitCompressStream();
 
-        while (k < bytes.length)
+        for (byte b : bytes)
         {
-            // probability of 0 in percent
-            int probability = 100 - (int) ((bitCount(bytes, k, DECOMPRESSED_WORD_SIZE) / (double) (DECOMPRESSED_WORD_SIZE * 8)) * 100);
-            double value = 0.0;
+            int i = 4;
 
-            // probability p of 0 in decimal
-            double p = (double) probability / 100;
-            double u = 0;
-            double v = 1;
-
-            do
+            while (i > 0)
             {
-                for (int i = 7; i >= 0; i--)
-                {
-                    int bit = getBit(bytes[k], i);
-                    double bound = u + (v - u) * p;
-
-                    if (bit == 0)
-                    {
-                        v = bound;
-                    }
-                    else
-                    {
-                        u = bound;
-                    }
-                }
-                k++;
+                i--;
+                int word = (b >> 2 * i) & 0x03;
+                bits.append((byte) dict.encode(word));
             }
-            while (k % DECOMPRESSED_WORD_SIZE != 0 && k < bytes.length);
-
-            value = u + (v - u) * p;
-            // prepend probability
-            buffer.put((byte) probability);
-            // prepend compressed block
-            buffer.putDouble(value);
         }
 
+        byte[] buf = new byte[5 + bits.size()];
+        ByteBuffer buffer = ByteBuffer.wrap(buf);
+        buffer.put(dict.toByte());
+        buffer.putInt(bytes.length);
+        buffer.put(bits.bytes());
         return buffer.array();
     }
 
+    /**
+     * Decompresses the given byte array
+     *
+     * @param bytes to decompress
+     *
+     * @return decompressed bytes
+     */
     public byte[] decompress(byte[] bytes)
     {
-        int len = bytes.length / COMPRESSED_WORD_SIZE * DECOMPRESSED_WORD_SIZE;
-        byte[] buf = new byte[len];
-        ByteBuffer buffer = ByteBuffer.wrap(buf);
-        int k = 0;
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        Dictionary dict = new Dictionary(buffer.get());
+        // length of the decompressed byte array
+        int len = buffer.getInt();
+        BitDecompressStream bits = new BitDecompressStream(Arrays.copyOfRange(bytes, 5, bytes.length));
+        byte[] out = new byte[len];
 
-        while (k < bytes.length)
+        for (int q = 0; !bits.hasNext() && q < len; q++)
         {
-            // probability of 0 in percent
-            int probability = (int) bytes[k];
-            // increase index
-            k++;
-            // probability of 0 in decimal
-            double p = (double) probability / 100;
-            double u = 0;
-            double v = 1;
-            double value = ByteBuffer.wrap(Arrays.copyOfRange(bytes, k, k + Double.SIZE / 8)).getDouble();
+            byte b = 0;
 
-            for (int j = 0; j < DECOMPRESSED_WORD_SIZE; j++)
+            for (int i = 3; i >= 0 && !bits.hasNext(); i--)
             {
-                int b = 0;
+                int code = 0;
+                int k = 0;
+                do
+                {
+                    int value = bits.next() ? 1 : 0;
+                    code = (code << 1) | value;
+                    k++;
+                }
+                while (!dict.isCode(code) && k < 3);
 
+                b = (byte) ((b << 2) | dict.decode(code));
+            }
+            out[q] = b;
+        }
+        return out;
+    }
+
+    /**
+     * Bit stream of the compressed bytes
+     */
+    private class BitDecompressStream implements Iterator<Boolean>
+    {
+        private LinkedList<Boolean> bits = new LinkedList<>();
+
+        public BitDecompressStream(byte[] bytes)
+        {
+            for (byte b : bytes)
+            {
                 for (int i = 7; i >= 0; i--)
                 {
-                    double bound = u + (v - u) * p;
-
-                    if (value <= bound)
-                    {
-                        v = bound;
-                    }
-                    else
-                    {
-                        b += (int) Math.pow(2, i);
-                        u = bound;
-                    }
+                    boolean value = ((b >> i) & 1) == 1 ? true : false;
+                    bits.add(value);
                 }
-
-                buffer.put((byte) b);
             }
-            k += Double.SIZE / 8;
         }
 
-        return buffer.array();
+        /**
+         * @return next bit
+         */
+        public Boolean next()
+        {
+            return bits.pop();
+        }
+
+        /**
+         * @return true if the stream is not empty
+         */
+        public boolean hasNext()
+        {
+            return !(bits.size() > 0);
+        }
+
+        /**
+         * not implemented
+         */
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
     }
 
-    public static int bitCount(byte[] bytes, int index, int len)
+    /**
+     * Bit stream of bytes being compressed
+     */
+    private class BitCompressStream
     {
-        return BitSet.valueOf(Arrays.copyOfRange(bytes, index, index + len)).cardinality();
+        private LinkedList<Boolean> bits = new LinkedList<>();
+
+        /**
+         * appends the given code to the stream
+         *
+         * @param word to append
+         */
+        public void append(byte word)
+        {
+            for (int i = 7; i >= 0; i--)
+            {
+                boolean value = ((word >> i) & 1) == 1 ? true : false;
+
+                if (value || i == 0)
+                {
+                    bits.add(value);
+                }
+            }
+        }
+
+        /**
+         * @return size of the bit set in bytes
+         */
+        public int size()
+        {
+            int value = (bits.size() % 8) != 0 ? 8 - (bits.size() % 8) : 0;
+            return (bits.size() + value) / 8;
+        }
+
+        /**
+         * maps the bit stream into a byte array
+         *
+         * @return byte array
+         */
+        public byte[] bytes()
+        {
+            byte[] bytes = new byte[size()];
+            int i = 0;
+            Iterator<Boolean> it = bits.iterator();
+
+            while (it.hasNext())
+            {
+                int k = 8;
+                byte b = 0;
+
+                while (it.hasNext() && k > 0)
+                {
+                    k--;
+                    boolean bit = it.next();
+                    int value = bit ? 1 : 0;
+                    b = (byte) ((value << k) | b);
+                }
+
+                bytes[i] = b;
+                i++;
+            }
+            return bytes;
+        }
     }
 
-    public static int getBit(byte b, int pos)
+    /**
+     * Dictionary to provide mapping of the Huffman codes
+     */
+    private class Dictionary
     {
-        return (b >> pos) & 1;
+        private int[] dictionary = new int[4];
+        private int[] order = new int[4];
+
+        /**
+         * Instantiate a new Dictionary and generate the mapping based on a
+         * statistical analysis of the bytes to being compressed.
+         *
+         * @param bytes to compress
+         */
+        public Dictionary(byte[] bytes)
+        {
+            int[] count = new int[4];
+
+            for (byte b : bytes)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    int word = (b >> 2 * i) & 0x03;
+                    count[word]++;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int max = 0;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    if (count[j] > count[max])
+                    {
+                        max = j;
+                    }
+                }
+                count[max] = -1;
+                dictionary[max] = CODE_BOOK[i];
+                order[i] = max;
+            }
+        }
+
+        /**
+         * Instantiate a new Dictionary based on the given one
+         *
+         * @param b given dictionary
+         */
+        public Dictionary(byte b)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int word = (b >> 2 * i) & 0x03;
+                dictionary[word] = CODE_BOOK[i];
+            }
+        }
+
+        /**
+         * @return dictionary represented as single byte
+         */
+        public byte toByte()
+        {
+            byte b = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                b |= (byte) (order[i] << (2 * i));
+            }
+            return b;
+        }
+
+        /**
+         * @param word to encode
+         *
+         * @return code that maps to the given word
+         */
+        public int encode(int word)
+        {
+            return dictionary[word];
+        }
+
+        /**
+         * @param code to decode
+         *
+         * @return word decoded from the given code
+         */
+        public int decode(int code)
+        {
+            for (int i = 0; i < dictionary.length; i++)
+            {
+                if (dictionary[i] == code)
+                {
+                    return i;
+                }
+            }
+            return Integer.MIN_VALUE;
+        }
+
+        /**
+         * @param code to check
+         *
+         * @return true if the given code was found in the dictionary, false otherwise
+         */
+        public boolean isCode(int code)
+        {
+            for (int value : dictionary)
+            {
+                if (value == code)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
--- VFS/test/ch/ethz/jcd/main/utils/VCompressorTest.java
+++ VFS/test/ch/ethz/jcd/main/utils/VCompressorTest.java
@@ -3,7 +3,7 @@ package ch.ethz.jcd.main.utils;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class VCompressorTest
 {
@@ -19,10 +19,52 @@ public class VCompressorTest
         VCompressor compressor = new VCompressor();
         //byte[] bytes = new byte[]{(byte) 0xF0, (byte) 0x0F, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
         //byte[] bytes = new byte[]{(byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55, (byte) 0x55};
-        byte[] bytes = new byte[]{(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
-        byte[] compressed = compressor.compress(bytes);
-        byte[] decompressed = compressor.decompress(compressed);
-        assertArrayEquals(bytes, decompressed);
+        //byte[] bytes = new byte[]{(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05,  (byte) 0xFE, (byte) 0xFF, (byte) 0xAF, (byte) 0xFF, (byte) 0xCF, (byte) 0xF3};
+        //byte[] bytes = new byte[]{(byte) 0x01, (byte) 0x00, (byte) 0xFF};
+        /*byte[] simple = new byte[]{(byte) 0x1B, (byte) 0x00};
+        byte[] a = compressor.compress(simple);
+        byte[] b = compressor.decompress(a);
+        assertArrayEquals(simple, b);
+        */
+        long comp = 0;
+        long total = 0;
+        int count = 1024;
+        byte[] bytes = new byte[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int k = 0; k < 256; k++)
+            {
+                byte[] compressed = compressor.compress(bytes);
+                byte[] decompressed = compressor.decompress(compressed);
+                assertArrayEquals(bytes, decompressed);
+                bytes[i] = (byte) (bytes[i] + 1);
+                total += bytes.length;
+                comp += compressed.length;
+            }
+            System.out.println(i + " / " + count);
+        }
+
+        bytes = new byte[count];
+
+        for (int i = 0; i < 256; i++)
+        {
+            for (int k = 0; k < count; k++)
+            {
+                byte[] compressed = compressor.compress(bytes);
+                byte[] decompressed = compressor.decompress(compressed);
+                assertArrayEquals(bytes, decompressed);
+                bytes[k] = (byte) (bytes[k] + 1);
+                total += bytes.length;
+                comp += compressed.length;
+            }
+            System.out.println(i + " / " + 256);
+        }
+
+        System.out.println("-------------");
+        System.out.println("total size: " + total);
+        System.out.println("compressed size: " + comp);
+        System.out.println("bytes saved: " + (total - comp)/(1024*265*2) );
     }
 
     @Test
